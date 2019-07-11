72d6e01798d740da63ab7c0eda5756c65dfc1f2b
Jayvee <jayvee.r.abella@gmail.com>
2019-07-11 15:51:53 -0500

    modeller scripts are now callable as modules

--- modeller_scripts/model_receptor.py
+++ modeller_scripts/model_receptor.py
@@ -1,12 +1,15 @@
 from subprocess import call
 import time
 import sys
+import os
+
+defaults_location = os.path.dirname(os.path.abspath(__file__))
 
 # python model_receptor.py <file containing alpha chain sequence> <template pdb>
 
 def main(args):
 
-	f = open(sys.argv[1], 'r')
+	f = open(args[0], 'r')
 	f_str = ""
 	for line in f:
 		f_str += line
@@ -15,14 +18,14 @@ def main(args):
 	target_sequence += "/"
 	# constant beta immunoglobin
 	target_sequence += "MIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLSFSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM*" 
-	receptor_template = sys.argv[2]
+	receptor_template = args[1]
 
 
 
 	print("Preparing receptor template")
 	call(["cp " + receptor_template + " receptor_template.pdb"], shell=True)
 	call(["sed -i \"/[A-Z] C  /d\" receptor_template.pdb"], shell=True)
-	call(["python mod_getseq.py > log.txt"], shell=True) # creates receptor_template.seq
+	call(["python " + defaults_location + "/mod_getseq.py > log.txt"], shell=True) # creates receptor_template.seq
 
 	print("Preparing target sequence")
 	f = open("target_sequence.pir", 'w')
@@ -33,13 +36,13 @@ def main(args):
 
 	print("Aligning target sequence with receptor template")
 	starttime = time.time()
-	call(["python align2d.py >> log.txt"], shell=True) # creates target_sequence-receptor_template.ali
+	call(["python " + defaults_location + "/align2d.py >> log.txt"], shell=True) # creates target_sequence-receptor_template.ali
 	endtime = time.time()
 	print("Aligning took " + str(endtime - starttime) + " seconds.")
 
 	print("Creating model")
 	starttime = time.time()
-	call(["python -u model-single-opt.py 2 >> log.txt"], shell=True)
+	call(["python -u  " + defaults_location + "/model-single-opt.py 2 >> log.txt"], shell=True)
 	call(["cp $(grep \"Top model\" log.txt | awk '{ print $3 }') best_model.pdb"], shell=True)
 	endtime = time.time()
 	print("Homology modelling took " + str(endtime - starttime) + " seconds.")
