0d7f50f8d8f67dfff5b647d7dc867c661e90f3fb
jayveeabella <j.abella@utexas.edu>
2019-07-12 14:57:10 -0500

    removing excess modeller files

--- modeller_scripts/mod_getseq.py
+++
@@ -1,8 +0,0 @@
-from modeller import *
-# Get the sequence of the PDB file, and write to an alignment file
-code = 'receptor_template'
-e = environ()
-m = model(e, file=code)
-aln = alignment(e)
-aln.append_model(m, align_codes=code)
-aln.write(file=code+'.seq')
